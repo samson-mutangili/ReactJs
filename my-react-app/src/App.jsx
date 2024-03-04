00187b73166b0292a3305c662a96bd0923efd371
samson-mutangili <samsonkimole@gmail.com>
2024-03-05 01:46:54 +0300

    Made a reusable list component

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -9,6 +9,20 @@ import List from './List.jsx';
 
 function App() {
 
+  const fruits = [
+                    {id: 1, name: "Apple", calories: 95}, 
+                    {id: 2, name: "Orange", calories: 45}, 
+                    {id: 3, name: "Banana", calories: 30}, 
+                    {id: 4, name: "Watermelon", calories: 70},
+                ]
+
+  const vegetables = [
+                  {id: 5, name: "Cabbage", calories: 75}, 
+                  {id: 6, name: "Spinach", calories: 145}, 
+                  {id: 7, name: "Potatoes", calories: 110}, 
+                  {id: 8, name: "Carrots", calories: 25},
+              ]
+
   return(
     <>
       <Header/>
@@ -22,7 +36,8 @@ function App() {
       <Student name="Brian" age={23} profession="Teacher" hasMasters={true}/>
       <Student/> */}
 
-      <List/>
+      {fruits.length > 0 && <List items={fruits} category="Fruits"/>}
+      {vegetables.length > 0 ? <List items={vegetables} category="Vegetables"/> : null}
 
       <Footer/>
     </>
--- my-react-app/src/List.jsx
+++ my-react-app/src/List.jsx
@@ -1,34 +1,56 @@
-function List(){
+import PropTypes from 'prop-types'
+function List(props){
 
-    const fruits = [
-                    {id: 1, name: "Apple", calories: 95}, 
-                    {id: 2, name: "Orange", calories: 45}, 
-                    {id: 3, name: "Banana", calories: 30}, 
-                    {id: 4, name: "Watermelon", calories: 70},
-                ]
+    const category = props.category;
+    const itemLists = props.items;
+
+
+    // const fruits = [
+    //                 {id: 1, name: "Apple", calories: 95}, 
+    //                 {id: 2, name: "Orange", calories: 45}, 
+    //                 {id: 3, name: "Banana", calories: 30}, 
+    //                 {id: 4, name: "Watermelon", calories: 70},
+    //             ]
 
     //sort the strings
-    fruits.sort((a, b) => a.name.localeCompare(b.name)); //sort in alphabetical order
+   // fruits.sort((a, b) => a.name.localeCompare(b.name)); //sort in alphabetical order
     //fruits.sort((a, b) => b.name.localeCompare(a.name)); //sort in reverse alphabetical order
 
-    fruits.sort((a, b) => a.calories - b.calories); //numeric order or ascending order
-    fruits.sort((a, b) => b.calories - a.calories); //reverse numeric order or descending order
+    //fruits.sort((a, b) => a.calories - b.calories); //numeric order or ascending order
+    //fruits.sort((a, b) => b.calories - a.calories); //reverse numeric order or descending order
 
 
     //fruits that have calories less than 50
-    const lowCalFruits = fruits.filter((fruit) => fruit.calories < 50)
+    //const lowCalFruits = fruits.filter((fruit) => fruit.calories < 50)
 
     //fruits that have high calories than 50
-    const highCalFruits = fruits.filter((fruit) => fruit.calories >= 50)
+    //const highCalFruits = fruits.filter((fruit) => fruit.calories >= 50)
 
-    const listItems = fruits.map(fruit => <li key={fruit.id}>
-                                            {fruit.name}: &nbsp;
-                                            <b>{fruit.calories}</b>
+    const listItems = itemLists.map(item => <li key={item.id}>
+                                            {item.name}: &nbsp;
+                                            <b>{item.calories}</b>
                                                             </li>);
 
     return(
-        <ol>{listItems}</ol>
+        <>
+        <h3 className="list-category">{category}</h3>
+        <ol className="list-items">{listItems}</ol>
+        </>
     );
+}
+
+//set the default props
+List.defaultProps = {
+    category: "Category",
+    items: [],
+}
+
+//set the props types
+List.propTypes = {
+    category: PropTypes.string,
+    items: PropTypes.arrayOf(PropTypes.shape({id: PropTypes.number,
+                                                name: PropTypes.string,
+                                            calories: PropTypes.number})),
 }
 
 export default List
